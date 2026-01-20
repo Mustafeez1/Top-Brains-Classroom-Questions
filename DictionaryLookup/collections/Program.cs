3986919e55888ac923d0f2a9a65fd759067501a5
Mustafeez1 <mustafeez72777@gmail.com>
2026-01-20 12:25:43 +0530

    Fifth Question

---
+++ DictionaryLookup/collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, int> employees = new Dictionary<int, int>
+        {
+            {1, 20000},
+            {4, 40000},
+            {5, 15000}
+        };
+
+        int[] employeeId = {1, 4, 5};
+        int total_salary = 0;
+        foreach(var item in employeeId)
+        {
+            if (employees.ContainsKey(item))
+            {
+                total_salary += employees[item];
+            }
+        }
+        Console.WriteLine($"{total_salary}");
+ 
+    }
+
+}
