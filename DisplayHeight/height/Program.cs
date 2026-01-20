4bf86fa402bdfc7b0cb1f51ab1e610a9a89f3fed
Mustafeez1 <mustafeez72777@gmail.com>
2026-01-20 12:51:42 +0530

    Sixth Question

---
+++ DisplayHeight/height/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter heoight in centimeters(cm)");
+        int height = int.Parse(Console.ReadLine());
+
+        DisplayHeight(height);
+    }
+    public static void DisplayHeight(int height)
+    {
+        if(height < 150)
+        {
+            Console.WriteLine("Short");
+        }
+        else if (height >= 150 && height < 180)
+        {
+            Console.WriteLine("Average");
+        }
+        else
+        {
+            Console.WriteLine("Tall");
+        }
+    }
+}
