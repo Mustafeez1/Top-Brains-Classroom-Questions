3a5409b21c9c1dd422ea3e8f68268f238940cc8f
Mustafeez1 <mustafeez72777@gmail.com>
2026-01-20 16:12:22 +0530

    Seventh Question

---
+++ LargestNumber/LargestInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number");
+        int first = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number");
+        int second = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the third number");
+        int third = int.Parse(Console.ReadLine());
+
+        if(first > second && first > third)
+        {
+            Console.WriteLine($"{first} is larger than {second} & {third}");
+        }
+        else if(second > third && second > first)
+        {
+            Console.WriteLine($"{second} is larger than {first} & {third}");
+        }
+        else
+        {
+            Console.WriteLine($"{third} is larger than {first} & {second}");
+        }
+    }
+}
