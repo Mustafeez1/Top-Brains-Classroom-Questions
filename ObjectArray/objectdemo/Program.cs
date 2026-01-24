bd61e9d38ace73ff569656811fa792d60b60ef04
Mustafeez1 <mustafeez72777@gmail.com>
2026-01-24 23:37:55 +0530

    Question

---
+++ ObjectArray/objectdemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the length of the array");
+        object[] elements = {"abc", 'a', 1, 12, 33, 29, "khan"};
+        int sum =0;
+        foreach(object item in elements)
+        {
+            if(item is int val)
+            {
+                sum += val;
+            }
+        }
+        Console.WriteLine($"Sum of the integers is {sum}");
+    }
+}
